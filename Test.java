2aa955c0ead150ce31da32eed0cb5952f30dc3e6
TULIKA GHOSH <tulikagetsmails@gmail.com>
2022-03-08 11:46:41 -0500

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,41 +1,23 @@
 
 public class Test {
 	
-	public static int triangleNumber(int n) {
-        int sum = 0;
-        for (int i = 0; i <= n; i++)
-            sum += i;
-        return sum;
-    }
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
+	    int n = 40, firstTerm = 0, secondTerm = 1;
+	    System.out.println("Fibonacci even numbers");
 
-       
-        int j = 0; 
-        int n = 0; 
-        int numberOfDivisors = 0; 
-        
-        while (numberOfDivisors <= 500) {
+	    for (int i = 1; i <= n; ++i) {
+	      if(firstTerm%2==0)
+	      System.out.print(firstTerm + ", ");
 
-            
-            numberOfDivisors = 0;
-            j++;
-            n = triangleNumber(j);
-            
-         
-            for (int i = 1; i <= Math.sqrt(n); i++)
-                if (n % i == 0)
-                    numberOfDivisors++;
-            
-         
-            numberOfDivisors *= 2;
-        }
-        
-     
-         System.out.println(n);
+	      int nextTerm = firstTerm + secondTerm;
+	      firstTerm = secondTerm;
+	      secondTerm = nextTerm;
+	    }
+	  }
       
     }
 	
 	
 
-}
+
